1e8111bbec5399213ff6cf0d810cf161bd738d69
Patrick Organ <patrick@permafrost.dev>
2022-12-01 16:35:52 -0500

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 
 const options = {
     sourceMapsEnabled: false,
